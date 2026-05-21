76bde766fccf567c7fc73597873d27210e44bff2
usamabinmah12 <usamabinmahbub13@gmail.com>
2026-05-21 20:09:27 +0600

    Json Web Token (JWT) created using better Auth

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,6 +1,8 @@
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { jwt } from "better-auth/plugins";
+import { jwtClient } from "better-auth/client/plugins";
 const client = new MongoClient(process.env.MONGO_URI);
 const db = client.db('petnest');
 export const auth = betterAuth({
@@ -18,4 +20,16 @@ export const auth = betterAuth({
             clientSecret: process.env.GOOGLE_SECRET , 
         }, 
     },
+    session : {
+      cookieCache: {
+        enabled:true,
+        strategy: "jwt",
+        maxAge: 12 * 24 * 60 * 60
+      }
+    },
+    plugins: [
+         jwt()
+    ]
+     
+    
 });
